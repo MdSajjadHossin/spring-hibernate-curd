8039c193a0f4f85fa007fb1de35a4b50fa76f0a3
Md Sajjad Hossin <86626194+MdSajjadHossin@users.noreply.github.com>
2023-07-11 00:19:47 +0600

    displaying data form the database on the console

--- 01-cruddemo-student/cruddemo/src/main/java/com/shossin/cruddemo/dao/StudentDAO.java
+++ 01-cruddemo-student/cruddemo/src/main/java/com/shossin/cruddemo/dao/StudentDAO.java
@@ -2,8 +2,12 @@ package com.shossin.cruddemo.dao;
 
 import com.shossin.cruddemo.entity.Student;
 
+import java.util.List;
+
 public interface StudentDAO {
     void save(Student theStudent);
 
     Student findById(Integer id);
+
+    List<Student> findAll();
 }

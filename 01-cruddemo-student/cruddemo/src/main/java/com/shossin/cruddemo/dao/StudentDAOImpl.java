f2dfbc5fcefdbbc35ec0ac94f4a644c7936f5fd6
Md Sajjad Hossin <86626194+MdSajjadHossin@users.noreply.github.com>
2023-07-11 06:53:05 +0600

    deleting student form the database

--- 01-cruddemo-student/cruddemo/src/main/java/com/shossin/cruddemo/dao/StudentDAOImpl.java
+++ 01-cruddemo-student/cruddemo/src/main/java/com/shossin/cruddemo/dao/StudentDAOImpl.java
@@ -60,6 +60,16 @@ public class StudentDAOImpl implements StudentDAO{
     public void update(Student theStudent) {
         entityManager.merge(theStudent);
     }
+
+    @Override
+    @Transactional
+    public void delete(Integer id) {
+        // retrieve the student
+        Student theStudent = entityManager.find(Student.class, id);
+
+        //delete the student
+        entityManager.remove(theStudent);
+    }
 }
 
 
